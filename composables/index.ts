0dcb5621c05dcb6a124a86800a3b6239fa9b5af1
Prisberg <fille.risberg@live.se>
2023-10-01 19:13:32 +0200

    fix quiz creation update list

--- composables/index.ts
+++ composables/index.ts
@@ -2,8 +2,11 @@ import { useState } from "nuxt/app";
 
 export const useQuizzes = () => {
     return useState('quizzes', () => ([{
-        id: null,
-        title: "There are currently no quizzes stored on this browser",
-        questions: null
+        id: "",
+        title: "",
+        questions: [{
+            question: "",
+            answer: ""
+        }]
     }]));
 }
